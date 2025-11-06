db79e312d038b635e7acd347bd1da6c9cd108cde
PallaviTarke <palllavi.tarke@gmail.com>
2025-11-06 11:30:51 +0530

    changes in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,33 @@
-variable "aws_region" { default = "ap-south-1" }
-variable "project" { default = "tv-minikube" }
-variable "key_pair_name" { description = "Existing AWS key pair name" }
-variable "public_key" { description = "Public key content if creating a key pair", default = null }
-variable "create_key_pair" { type = bool, default = false }
-variable "ingress_http_port" { default = 30080 }
-variable "ingress_https_port" { default = 30443 }
+variable "aws_region" {
+  default     = "ap-south-1"
+  description = "AWS region for deployment"
+}
+
+variable "project" {
+  default     = "tv-minikube"
+  description = "Project name"
+}
+
+variable "key_pair_name" {
+  description = "Existing AWS key pair name"
+}
+
+variable "public_key" {
+  description = "Public key content if creating a key pair"
+  default     = null
+}
+
+variable "create_key_pair" {
+  type        = bool
+  default     = false
+}
+
+variable "ingress_http_port" {
+  default     = 30080
+  description = "HTTP NodePort for ingress"
+}
+
+variable "ingress_https_port" {
+  default     = 30443
+  description = "HTTPS NodePort for ingress"
+}
